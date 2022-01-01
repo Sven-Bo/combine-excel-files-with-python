23d9bb1b5c1e373317a961e9e7a84ea9b4834d41
Sven-Bo <jubbel3@gmx.net>
2022-01-01 13:33:21 +0100

    🎉 Init Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# How To Automate Excel Using Python | Combine Files & Create Charts 🤓
+
+In this tutorial, I will show you, how you can automate your excel tasks using python.
+In particular, I am going to show you how you can combine excel files & create charts out of them.
+
+## Video Tutorial
+
+[![YouTube Video](https://img.youtube.com/vi/daeMTChs-r4/0.jpg)](https://youtu.be/daeMTChs-r4)
+
+## Author
+
+- Sven from Coding Is Fun
+- YouTube: https://youtube.com/c/CodingIsFun
+- Website: https://pythonandvba.com
+
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/X7X47Q0EG)
+
+## Feedback
+
+If you have any feedback, please reach out to me at contact@pythonandvba.com
+![Logo](https://content.screencast.com/users/jubbel3/folders/Snagit/media/c42ea34b-4057-4754-96b0-e8e05c866afb/08.18.2021-19.56.png)
+
---
+++ combine_files.py
@@ -0,0 +1,38 @@
+import calendar
+import datetime as dt
+from pathlib import Path
+
+import pandas as pd
+import plotly
+import plotly.express as px
+
+BASE_DIR = Path(__file__).parent
+
+# Get all excel file paths
+files = list(BASE_DIR.rglob("*.xlsx*"))
+
+# Create empty DataFrame
+combined = pd.DataFrame()
+
+for file in files:
+    df = pd.read_excel(file)
+    df["Date"] = df["Date"].dt.date
+    df["Day"] = pd.DatetimeIndex(df["Date"]).day
+    df["Month"] = pd.DatetimeIndex(df["Date"]).month
+    df["Year"] = pd.DatetimeIndex(df["Date"]).year
+    df.dropna(inplace=True)
+    df["Month_Name"] = df["Month"].apply(lambda x: calendar.month_abbr[int(x)])
+    combined = combined.append(df, ignore_index=True)
+
+# Create Bar Chart
+fig = px.bar(combined, x="Month_Name", y="Sales", title="Sales 1Q 2020")
+
+# Save Bar Chart and Export to HTML
+plotly.offline.plot(fig, filename="Sales_1Q_2020.html")
+
+# Send Report as attached mail
+# Create more charts
+# ...
+
+# Export combined files to Excel
+combined.to_excel("Sales_1Q2020.xlsx", index=False, sheet_name="1Q 2020 Sales")
